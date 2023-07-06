769644ff3c8a386d9b4cdba2d47f6b00cff973fb
Eduardo Campos <edumachadocampos@gmail.com>
2023-07-05 22:09:38 -0300

    Some updates

--- src/App.js
+++ src/App.js
@@ -5,22 +5,16 @@ import SearchIcon from './search.svg';
 
 const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=eefd0977';
 
-const movie1 = {
-    "Title": "Amazing Spiderman Syndrome",
-    "Year": "2012",
-    "imdbID": "tt2586634",
-    "Type": "movie",
-    "Poster": "N/A"
-}
 
 const App = () => {
-    const [movies, setMovies] = useState();
+    const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
     
     const searchMovies = async (title) => {
         const response = await fetch('${API_URL}&s=${title}');
         const data = await response.json();
 
-        console.log(data.Search);
+        setMovies(data.Search);
     } 
 
     useEffect(() => {
@@ -33,18 +27,29 @@ const App = () => {
             <div className="search">
                 <input 
                   placeholder="Search for movies"
-                  value="Superman"
-                  onChange={() => {}}
+                  value={searchTerm}
+                  onChange={(e) => setSearchTerm(e.target.value)}
                 />
                 <img
                   src={SearchIcon}
                   alt="search"
-                  onClick={() =>{}}
+                  onClick={() => searchMovies(searchTerm)}
                 />
             </div>
-            <div className="container">
-                <MovieCard movie1={movie1} />;
-            </div>
+
+            {movies?.length > 0
+              ? (
+                    <div className="container">
+                        {movies.map((movie) => (
+                            <MovieCard movie={movie} />
+                        ))}
+                    </div>
+                  ) : (
+                    <div className="empty">
+                        <h2>No movies found</h2>
+                    </div>
+                  )
+            }
         </div>
     );
 }
